56132a5497dea8ad2ec28c3d78e7f23fef931965
AswinGopakumar-666 <aswingopakumar2610@gmail.com>
2025-04-14 14:25:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "Aviatrix_aswin"
+    organization = "Aviatrix_aswin"
     workspaces {
       name = "ace-iac-day-zero"
     }
